e7108d7b243ef3cd383d26e3a0365246268a2f78
kotfind <kotfind@yandex.ru>
2025-03-01 21:32:16 +0300

    Improve flake.nix

--- flake.nix
+++ flake.nix
@@ -1,116 +1,166 @@
 {
-    outputs = { nixpkgs, ... }:
-        let
-            system = "x86_64-linux";
-            pkgs = import nixpkgs {
-                inherit system;
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
 
-                config = {
-                    allowUnfree = true;
-                    android_sdk.accept_license = true;
-                };
-            };
-            lib = pkgs.lib;
+      config = {
+        allowUnfree = true;
+        android_sdk.accept_license = true;
+      };
+    };
+    lib = pkgs.lib;
 
-            buildToolsVersion = "34.0.0";
-            cmakeVersion = "3.10.2";
-            platformVersion = "34";
-            abiVersion = "x86_64";
-            systemImageType = "default";
+    buildToolsVersion = "34.0.0";
+    cmakeVersion = "3.10.2";
+    platformVersion = "34";
+    abiVersion = "x86_64";
+    systemImageType = "default";
 
-            androidComposition = pkgs.androidenv.composeAndroidPackages {
-                cmdLineToolsVersion = "8.0";
-                toolsVersion = "26.1.1";
-                platformToolsVersion = "33.0.3";
-                buildToolsVersions = [ buildToolsVersion ];
-                includeEmulator = true;
-                emulatorVersion = "34.2.16";
-                platformVersions = [ platformVersion ];
-                includeSources = false;
-                includeSystemImages = true;
-                systemImageTypes = [ systemImageType ];
-                abiVersions = [ abiVersion];
-                cmakeVersions = [ cmakeVersion ];
-                includeNDK = true;
-                ndkVersions = ["22.0.7026061"];
-                useGoogleAPIs = false;
-                useGoogleTVAddOns = false;
-                includeExtras = [];
-            };
+    androidComposition = pkgs.androidenv.composeAndroidPackages {
+      cmdLineToolsVersion = "8.0";
+      toolsVersion = "26.1.1";
+      platformToolsVersion = "33.0.3";
+      buildToolsVersions = [buildToolsVersion];
+      includeEmulator = true;
+      emulatorVersion = "34.2.16";
+      platformVersions = [platformVersion];
+      includeSources = false;
+      includeSystemImages = true;
+      systemImageTypes = [systemImageType];
+      abiVersions = [abiVersion];
+      cmakeVersions = [cmakeVersion];
+      includeNDK = true;
+      ndkVersions = ["22.0.7026061"];
+      useGoogleAPIs = false;
+      useGoogleTVAddOns = false;
+      includeExtras = [];
+    };
 
-            sdkDir = "${androidComposition.androidsdk}/libexec/android-sdk";
-            env = /* bash */ ''
-                export ANDROID_HOME="${sdkDir}"
-                export ANDROID_NDK_ROOT="${sdkDir}/ndk-bundle"
-                export ANDROID_SDK_ROOT="${sdkDir}"
+    env = let
+      sdkDir = "${androidComposition.androidsdk}/libexec/android-sdk";
+    in
+      /*
+      bash
+      */
+      ''
+        export ANDROID_HOME="${sdkDir}"
+        export ANDROID_NDK_ROOT="${sdkDir}/ndk-bundle"
+        export ANDROID_SDK_ROOT="${sdkDir}"
 
-                export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdkDir}/build-tools/${buildToolsVersion}/aapt2"
-            '';
+        export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdkDir}/build-tools/${buildToolsVersion}/aapt2"
+      '';
 
-            # Note: in `./.avd/device.avd/config.ini` set
-            # `hw.keyboard` and `hw.mainKeys` to `yes`.
-            # source: https://stackoverflow.com/a/64877532
-            emulator = pkgs.androidenv.emulateApp {
-                name = "emulator";
-                inherit platformVersion abiVersion systemImageType;
-                avdHomeDir = "./.avd";
-            };
+    emulatorScript = let
+      # Note: in `./.avd/device.avd/config.ini` set
+      # `hw.keyboard` and `hw.mainKeys` to `yes`.
+      # source: https://stackoverflow.com/a/64877532
+      emulator = pkgs.androidenv.emulateApp {
+        name = "emulator";
+        inherit platformVersion abiVersion systemImageType;
+        avdHomeDir = "./.avd";
+      };
+    in
+      pkgs.writeShellScriptBin "emulator" ''
+        ${env}
+        ${lib.getExe' emulator "run-test-emulator"}
+      '';
 
-            emulatorScript = pkgs.writeShellScriptBin "emulator" ''
-                ${env}
-                ${lib.getExe' emulator "run-test-emulator"}
-            '';
+    fhsEnv =
+      (pkgs.buildFHSEnv {
+        name = "Android Development Environment";
 
-            fhsEnv = (pkgs.buildFHSEnv {
-                name = "Android Development Environment";
+        targetPkgs = pkgs:
+          [
+            pkgs.gradle
+          ]
+          ++ (with androidComposition; [
+            platform-tools
+            tools
+          ]);
 
-                targetPkgs = pkgs:
-                    [
-                        pkgs.gradle
-                    ]
-                    ++ (with androidComposition; [
-                        platform-tools
-                        tools
-                    ]);
+        profile = env;
 
-                profile = env;
+        runScript = "bash";
+      })
+      .env;
 
-                runScript = "bash";
-            }).env;
+    runScript = let
+      adb = lib.getExe' androidComposition.platform-tools "adb";
+      awk = lib.getExe pkgs.gawk;
+      gradle = lib.getExe pkgs.gradle;
+      echo = lib.getExe pkgs.toybox "echo";
+    in
+      pkgs.writeShellScriptBin "run" ''
+        set -euo pipefail
+        set -o
 
-            adb = lib.getExe' androidComposition.platform-tools "adb";
-            awk = lib.getExe pkgs.gawk;
-            gradle = lib.getExe pkgs.gradle;
+        usage() {
+          ${echo} ${lib.escapeShellArg (
+          ""
+          + "Usage:\n"
+          + "    run [OPTIONS]\n"
+          + "\n"
+          + "Description:\n"
+          + "    Compiles the app and runs in on either\n"
+          + "    emulator (by default) on actual device (-d flag).\n"
+          + "\n"
+          + "Options:\n"
+          + "    -h    print this message\n"
+          + "    -d    try runnnig on non-emulator device\n"
+        )} 1>&2
+        }
 
-            runScript = pkgs.writeShellScriptBin "run" ''
-                set -euo pipefail
-                set -o
+        on_emulator=true
 
-                ANDROID_SERIAL="$(${adb} devices | ${awk} '/emulator/ { print $1; }')"
-                export ANDROID_SERIAL
+        while getopts ":hd" opt; do
+          case "$opt" in
+            'h')
+              usage
+              exit 0
+              ;;
 
-                if [ -z "$ANDROID_SERIAL" ]; then
-                    echo "error: no emulators are running"
-                    exit 1
-                fi
+            'd')
+              on_emulator=false
+              ;;
 
-                ${gradle} installDebug
-                ${adb} shell am start -n com.kotfind.android_course/.MainActivity
-            '';
-        in
-        {
-            devShells.${system}.default = fhsEnv;
+            *)
+              usage
+              exit 1
+              ;;
+          esac
+        done
 
-            apps.${system} = {
-                emulator = {
-                    type = "app";
-                    program = lib.getExe emulatorScript;
-                };
+        if [ "$on_emulator" == true ]; then
+          neg=""
+        else
+          neg="!"
+        fi
 
-                run = {
-                    type = "app";
-                    program = lib.getExe runScript;
-                };
-            };
-        };
+        ANDROID_SERIAL="$(${adb} devices | ${awk} "$neg/emulator/ { print \$1; }")"
+        export ANDROID_SERIAL
+
+        if [ -z "$ANDROID_SERIAL" ]; then
+          ${echo} "error: no devices (emulators) found"
+          exit 1
+        fi
+
+        ${gradle} installDebug
+        ${adb} shell am start -n com.kotfind.android_course/.MainActivity
+      '';
+  in {
+    devShells.${system}.default = fhsEnv;
+
+    apps.${system} = {
+      emulator = {
+        type = "app";
+        program = lib.getExe emulatorScript;
+      };
+
+      run = {
+        type = "app";
+        program = lib.getExe runScript;
+      };
+    };
+  };
 }
