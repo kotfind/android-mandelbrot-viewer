5cfc62a4af7b75dd062648024f335bd4a125d9e5
kotfind <kotfind@yandex.ru>
2025-03-03 00:47:18 +0300

    Move version info from build.gradle.kts to flake.nix

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,15 @@ plugins {
 
 android {
     namespace = "com.kotfind.android_course"
-    compileSdk = 34
+    compileSdk = env("CFG_VERSIONS_SDK_COMPILE")
 
     defaultConfig {
         applicationId = "com.kotfind.android_course"
         versionName = "1.0"
         versionCode = 1
 
-        minSdk = 30
-        targetSdk = 34
+        minSdk = env("CFG_VERSIONS_SDK_MIN")
+        targetSdk = env("CFG_VERSIONS_SDK_TARGET")
 
         vectorDrawables { useSupportLibrary = true }
     }
@@ -29,14 +29,18 @@ android {
     buildTypes { release { isMinifyEnabled = false } }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = env("CFG_VERSIONS_JVM_TARGET")
+        targetCompatibility = env("CFG_VERSIONS_JVM_TARGET")
     }
 
-    kotlinOptions { jvmTarget = "1.8" }
+    kotlinOptions { jvmTarget = env("CFG_VERSIONS_JVM_TARGET") }
     buildFeatures { compose = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.1" }
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
+
+    buildToolsVersion = env("CFG_VERSIONS_BUILD_TOOLS")
+    ndkVersion = env("CFG_VERSIONS_NDK")
+    externalNativeBuild { cmake { version = env("CFG_VERSIONS_CMAKE") } }
 }
 
 dependencies {
@@ -47,4 +51,29 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
+}
+
+inline fun <reified T> env(env_name: String): T {
+    val env_val = System.getenv(env_name)
+
+    if (env_val == null) {
+        throw GradleException("$env_name not set: did you forget to enter devShell?")
+    }
+
+    try {
+        return when (T::class) {
+            Int::class -> env_val.toInt() as T
+            String::class -> env_val as T
+            JavaVersion::class -> JavaVersion.valueOf("VERSION_" + env_val.replace(".", "_")) as T
+
+            else -> throw GradleException("Unsupported env type ${T::class.simpleName}")
+        }
+    } catch (e: GradleException) {
+        throw e
+    } catch (e: Exception) {
+        throw GradleException(
+            "$env_name is set to value '$env_val', which " +
+                "cannot be parsed to ${T::class.simpleName}",
+            e)
+    }
 }
--- flake.nix
+++ flake.nix
@@ -11,55 +11,117 @@
     };
     lib = pkgs.lib;
 
-    buildToolsVersion = "34.0.0";
-    cmakeVersion = "3.10.2";
-    platformVersion = "34";
-    abiVersion = "x86_64";
-    systemImageType = "default";
+    # To check available versions use
+    #     sdkmanager --list
+    # or
+    #     just specify random one and check the error message
+    cfg = {
+      versions = {
+        # SDK to Android version mapping:
+        #     https://developer.android.com/tools/releases/platforms
+        # Difference between compileSdk, targetSdk and minSdk:
+        #     https://stackoverflow.com/a/47269079
+        sdk = rec {
+          compile = "34";
+          target = "34";
+          min = "30";
 
-    androidComposition = pkgs.androidenv.composeAndroidPackages {
-      cmdLineToolsVersion = "8.0";
-      toolsVersion = "26.1.1";
-      platformToolsVersion = "33.0.3";
-      buildToolsVersions = [buildToolsVersion];
+          # Setting it to compile-sdk is the easiest way, as compile-sdk
+          # is the only one, that is installed anyway.
+          emulator = compile;
+        };
+
+        # x86_64 allows running emulator on x86_64 machine,
+        # but is likely incompable with your physical device
+        abi = "x86_64";
+
+        jvm-target = "1.8";
+
+        # just set those to the last available version (I guess?)
+        build-tools = "35.0.1";
+        cmdline-tools = "9.0";
+        platform-tools = "35.0.2";
+        cmake = "3.31.4";
+        emulator = "35.4.6";
+        ndk = "22.1.7171670";
+      };
+      system-image-type = "default";
+    };
+
+    androidComposition = pkgs.androidenv.composeAndroidPackages (with cfg.versions; {
+      cmdLineToolsVersion = cmdline-tools;
+      toolsVersion = null;
+      platformToolsVersion = platform-tools;
+      buildToolsVersions = [build-tools];
       includeEmulator = true;
-      emulatorVersion = "34.2.16";
-      platformVersions = [platformVersion];
+      emulatorVersion = emulator;
+      # platformVersions seems to misbehave if two same versions are specified
+      platformVersions =
+        if sdk.compile == sdk.emulator
+        then [sdk.compile]
+        else [sdk.emulator sdk.compile];
       includeSources = false;
       includeSystemImages = true;
-      systemImageTypes = [systemImageType];
-      abiVersions = [abiVersion];
-      cmakeVersions = [cmakeVersion];
+      systemImageTypes = [cfg.system-image-type];
+      abiVersions = [abi];
+      cmakeVersions = [cmake];
       includeNDK = true;
-      ndkVersions = ["22.0.7026061"];
+      ndkVersions = [ndk];
       useGoogleAPIs = false;
       useGoogleTVAddOns = false;
       includeExtras = [];
-    };
+    });
 
     env = let
       sdkDir = "${androidComposition.androidsdk}/libexec/android-sdk";
+      ndkDir = "${sdkDir}/ndk-bundle";
+
+      cfgExportMap =
+        lib.attrsets.mapAttrsRecursive
+        (
+          path: value: let
+            envName =
+              lib.strings.concatMapStringsSep "_" (
+                pathFrag: builtins.replaceStrings ["-"] ["_"] (lib.strings.toUpper pathFrag)
+              )
+              path;
+            envVal = lib.escapeShellArg (
+              builtins.toString value
+            );
+          in "export ${envName}=${envVal}"
+        )
+        {inherit cfg;};
+
+      cfgExportMapVals = lib.attrsets.collect builtins.isString cfgExportMap;
+
+      cfgExport = lib.strings.concatStringsSep "\n" cfgExportMapVals;
     in
       /*
       bash
       */
       ''
         export ANDROID_HOME="${sdkDir}"
-        export ANDROID_NDK_ROOT="${sdkDir}/ndk-bundle"
+        export ANDROID_NDK_ROOT="${ndkDir}"
         export ANDROID_SDK_ROOT="${sdkDir}"
 
-        export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdkDir}/build-tools/${buildToolsVersion}/aapt2"
+        export GRADLE_OPTS="-Dorg.gradle.project.android.aapt2FromMavenOverride=${sdkDir}/build-tools/${cfg.versions.build-tools}/aapt2"
+
+        ${cfgExport}
       '';
 
     emulatorScript = let
       # Note: in `./.avd/device.avd/config.ini` set
       # `hw.keyboard` and `hw.mainKeys` to `yes`.
       # source: https://stackoverflow.com/a/64877532
-      emulator = pkgs.androidenv.emulateApp {
-        name = "emulator";
-        inherit platformVersion abiVersion systemImageType;
-        avdHomeDir = "./.avd";
-      };
+      emulator = pkgs.androidenv.emulateApp ({
+          name = "emulator";
+          avdHomeDir = "./.avd";
+        }
+        // (with cfg.versions; {
+          platformVersion = sdk.emulator;
+          abiVersion = abi;
+          systemImageType = cfg.system-image-type;
+        }));
     in
       pkgs.writeShellScriptBin "emulator" ''
         ${env}
@@ -75,8 +137,8 @@
             pkgs.gradle
           ]
           ++ (with androidComposition; [
+            androidsdk
             platform-tools
-            tools
           ]);
 
         profile = env;
