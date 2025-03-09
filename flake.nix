d21ad06b3018d3ee334872e5686aca465c5f0bc9
kotfind <kotfind@yandex.ru>
2025-03-09 11:03:41 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
       includeExtras = [];
     });
 
-    env = let
+    envActivationCode = let
       sdkDir = "${androidComposition.androidsdk}/libexec/android-sdk";
       ndkDir = "${sdkDir}/ndk-bundle";
 
@@ -120,25 +120,6 @@
         ${cfgExport}
       '';
 
-    emulatorScript = let
-      # Note: in `./.avd/device.avd/config.ini` set
-      # `hw.keyboard` and `hw.mainKeys` to `yes`.
-      # source: https://stackoverflow.com/a/64877532
-      emulator = pkgs.androidenv.emulateApp ({
-          name = "emulator";
-          avdHomeDir = "./.avd";
-        }
-        // (with cfg.versions; {
-          platformVersion = sdk.emulator;
-          abiVersion = abi;
-          systemImageType = cfg.system-image-type;
-        }));
-    in
-      pkgs.writeShellScriptBin "emulator" ''
-        ${env}
-        ${lib.getExe' emulator "run-test-emulator"}
-      '';
-
     fhsEnv =
       (pkgs.buildFHSEnv {
         name = "Android Development Environment";
@@ -152,39 +133,109 @@
             platform-tools
           ]);
 
-        profile = env;
+        profile = envActivationCode;
 
         runScript = "bash";
       })
       .env;
 
-    runScript = let
-      adb = lib.getExe' androidComposition.platform-tools "adb";
-      awk = lib.getExe pkgs.gawk;
-      gradle = lib.getExe pkgs.gradle;
-      echo = lib.getExe' pkgs.toybox "echo";
+    emulatorScript = let
+      # Note: in `./.avd/device.avd/config.ini` set
+      # `hw.keyboard` and `hw.mainKeys` to `yes`.
+      # source: https://stackoverflow.com/a/64877532
+      emulatorPkg = pkgs.androidenv.emulateApp ({
+          name = "emulator";
+          avdHomeDir = "./.avd";
+        }
+        // (with cfg.versions; {
+          platformVersion = sdk.emulator;
+          abiVersion = abi;
+          systemImageType = cfg.system-image-type;
+        }));
+    in
+      pkgs.writeShellScriptBin "emulator" ''
+        ${envActivationCode}
+        ${lib.getExe' emulatorPkg "run-test-emulator"}
+      '';
 
+    genRunScript = {
+      # name of generated script
+      scriptName,
+      # gradle subcommand like "install" or "assemble"
+      gradleAction,
+      # build type: "debug" or "release"
+      buildType ? "Debug",
+      # whatever or not to run app after installation
+      doRun ? true,
+      # "emulator", "device" or "none"
+      deviceType ? "emulator",
+    }: let
       firstLetterToUpper = str:
         lib.strings.toUpper (builtins.substring 0 1 str)
         + builtins.substring 1 ((builtins.stringLength str) - 1) str;
 
-      gradleSubCommand = "install" + firstLetterToUpper cfg.app.flavor + "Debug";
       fullPackageName = with cfg.app; package + "." + flavor;
+
+      gradleSubcommand = "${gradleAction}${
+        lib.escapeShellArg (firstLetterToUpper cfg.app.flavor)
+      }${
+        lib.escapeShellArg (firstLetterToUpper buildType)
+      }";
+
+      adb = lib.getExe' androidComposition.platform-tools "adb";
+      awk = lib.getExe pkgs.gawk;
+      gradle = lib.getExe pkgs.gradle;
+      echo = lib.getExe' pkgs.toybox "echo";
+      head = lib.getExe' pkgs.toybox "head";
+      tail = lib.getExe' pkgs.toybox "tail";
+
+      awkDeviceNameFilter =
+        (
+          if deviceType == "emulator"
+          then ""
+          else "!"
+        )
+        + "/emulator/";
+
+      notFoundErrorMsg = "error: no ${
+        if deviceType == "emulator"
+        then "emulators"
+        else "devices"
+      } found";
+
+      setAndroidSerialCmd =
+        if deviceType == "emulator" || deviceType == "device"
+        then
+          /*
+          bash
+          */
+          ''
+            ANDROID_SERIAL="$( \
+              ${adb} devices | \
+              ${tail} -n +2 | \
+              ${awk} '${awkDeviceNameFilter} { print $1; }' | \
+              ${head} -n 1)"
+            export ANDROID_SERIAL
+
+            if [ -z "$ANDROID_SERIAL" ]; then
+              ${echo} ${lib.escapeShellArg notFoundErrorMsg}
+              exit 1
+            fi
+          ''
+        else "";
+
+      runCommand =
+        if doRun
+        then "${adb} shell monkey -p ${fullPackageName} -c android.intent.category.LAUNCHER 1"
+        else "";
     in
-      pkgs.writeShellScriptBin "run" ''
+      pkgs.writeShellScriptBin scriptName ''
         set -euo pipefail
         set -o
 
-        ANDROID_SERIAL="$(${adb} devices | ${awk} "/emulator/ { print \$1; }")"
-        export ANDROID_SERIAL
-
-        if [ -z "$ANDROID_SERIAL" ]; then
-          ${echo} "error: no emulators found"
-          exit 1
-        fi
-
-        ${gradle} ${gradleSubCommand}
-        ${adb} shell monkey -p ${fullPackageName} -c android.intent.category.LAUNCHER 1
+        ${setAndroidSerialCmd}
+        ${gradle} ${gradleSubcommand}
+        ${runCommand}
       '';
   in {
     devShells.${system}.default = fhsEnv;
@@ -197,7 +248,33 @@
 
       run = {
         type = "app";
-        program = lib.getExe runScript;
+        program = lib.getExe (genRunScript {
+          scriptName = "run";
+          gradleAction = "install";
+          buildType = "debug";
+          deviceType = "emulator";
+        });
+      };
+
+      install = {
+        type = "app";
+        program = lib.getExe (genRunScript {
+          scriptName = "install";
+          gradleAction = "install";
+          buildType = "debug";
+          deviceType = "device";
+        });
+      };
+
+      assemble = {
+        type = "app";
+        program = lib.getExe (genRunScript {
+          scriptName = "assemble";
+          gradleAction = "assemble";
+          buildType = "debug";
+          deviceType = "none";
+          doRun = false;
+        });
       };
     };
   };
